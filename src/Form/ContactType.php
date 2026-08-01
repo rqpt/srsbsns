8dfa64d41ccce53c2e935005c0ad856955b91a0e
Pieter Ernst Vermeulen <contact@pevermeulen.com>
2026-08-01 20:33:03 -0200

    Change Contact form email input to EmailType

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -17,7 +18,7 @@ class ContactType extends AbstractType
             ->add('name', TextType::class)
             ->add('surname', TextType::class)
             ->add('phone_number', TelType::class)
-            ->add('email', TextType::class);
+            ->add('email', EmailType::class)
     }
 
     public function configureOptions(OptionsResolver $resolver): void
