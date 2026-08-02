aca3f73b046abe57f103f2443e9eaf9ea7254e38
Pieter Ernst Vermeulen <contact@pevermeulen.com>
2026-08-02 11:33:21 -0200

    Delete old Contact mail implementation

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,13 +6,10 @@ use App\Entity\Contact;
 use App\Form\ContactType;
 use Doctrine\ORM\EntityManagerInterface;
 use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3Validator;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route(
@@ -25,10 +22,8 @@ final class ContactController extends AbstractController
     public function __invoke(
         Request $request,
         EntityManagerInterface $entityManager,
-        MailerInterface $mailer,
         Recaptcha3Validator $recaptcha3Validator,
         #[Autowire('%karser_recaptcha3.score_threshold%')] float $recaptchaThreshold,
-        #[Autowire('%app.admin_email%')] string $adminEmail,
     ): Response {
         $contact = new Contact;
 
@@ -49,7 +44,6 @@ final class ContactController extends AbstractController
             $entityManager->persist($contact);
             $entityManager->flush();
 
-            $this->sendNotificationMails($contact, $mailer, $adminEmail);
 
             $this->addFlash('success', sprintf(
                 <<<'FLASH'
@@ -72,26 +66,5 @@ final class ContactController extends AbstractController
             'contact/index.html.twig',
             compact('form', 'contacts'),
         );
-    }
-
-    private function sendNotificationMails(
-        Contact $contact,
-        MailerInterface $mailer,
-        string $adminEmail,
-    ): void {
-        $emailToContact = (new TemplatedEmail)
-            ->to(Address::create($contact->getEmail()))
-            ->subject('Contact saved')
-            ->htmlTemplate('emails/contact_confirmation.html.twig')
-            ->context(['contact' => $contact]);
-
-        $emailToAdmin = (new TemplatedEmail)
-            ->to(Address::create($adminEmail))
-            ->subject('New Contact Submission')
-            ->htmlTemplate('emails/contact_admin_notification.html.twig')
-            ->context(['contact' => $contact]);
-
-        $mailer->send($emailToContact);
-        $mailer->send($emailToAdmin);
     }
 }
