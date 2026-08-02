11faa44e174d0644c461b82850cdcecf22c51663
Pieter Ernst Vermeulen <contact@pevermeulen.com>
2026-08-01 23:08:55 -0200

    Add notifier to run on contact creation

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route(
@@ -20,6 +22,8 @@ final class ContactController extends AbstractController
     public function __invoke(
         Request $request,
         EntityManagerInterface $entityManager,
+        MailerInterface $mailer,
+        string $adminEmail,
     ): Response {
         $contact = new Contact;
 
@@ -29,6 +33,8 @@ final class ContactController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($contact);
             $entityManager->flush();
+
+            $this->sendNotificationMails($contact, $mailer, $adminEmail);
 
             $this->addFlash('success', 'Contact created successfully!');
 
@@ -43,5 +49,26 @@ final class ContactController extends AbstractController
             'contact/index.html.twig',
             compact('form', 'contacts'),
         );
+    }
+
+    private function sendNotificationMails(
+        Contact $contact,
+        MailerInterface $mailer,
+        string $adminEmail,
+    ): void {
+        $emailToContact = (new TemplatedEmail)
+            ->to($contact->getEmail())
+            ->subject('Contact saved')
+            ->htmlTemplate('emails/contact_confirmation.html.twig')
+            ->context(['contact' => $contact]);
+
+        $emailToAdmin = (new TemplatedEmail)
+            ->to($adminEmail)
+            ->subject('New Contact Submission')
+            ->htmlTemplate('emails/contact_admin_notification.html.twig')
+            ->context(['contact' => $contact]);
+
+        $mailer->send($emailToContact);
+        $mailer->send($emailToAdmin);
     }
 }
