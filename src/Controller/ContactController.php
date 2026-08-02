ab006d6c2469bb15557a917e1e66941768cb3970
Pieter Ernst Vermeulen <contact@pevermeulen.com>
2026-08-02 10:28:47 -0200

    Add Contact form reCAPTCHA score and threshold to success flash message

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,11 +5,14 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use Doctrine\ORM\EntityManagerInterface;
+use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3Validator;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route(
@@ -23,6 +26,8 @@ final class ContactController extends AbstractController
         Request $request,
         EntityManagerInterface $entityManager,
         MailerInterface $mailer,
+        Recaptcha3Validator $recaptcha3Validator,
+        #[Autowire('%karser_recaptcha3.score_threshold%')] float $recaptchaThreshold,
         #[Autowire('%app.admin_email%')] string $adminEmail,
     ): Response {
         $contact = new Contact;
@@ -30,13 +35,31 @@ final class ContactController extends AbstractController
         $form = $this->createForm(ContactType::class, $contact);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        $formIsSubmitted = $form->isSubmitted();
+
+        $recaptchaScore = null;
+
+        if ($formIsSubmitted) {
+            $recaptchaScore = $recaptcha3Validator
+                ->getLastResponse()
+                ?->getScore();
+        }
+
+        if ($formIsSubmitted && $form->isValid()) {
             $entityManager->persist($contact);
             $entityManager->flush();
 
             $this->sendNotificationMails($contact, $mailer, $adminEmail);
 
-            $this->addFlash('success', 'Contact created successfully!');
+            $this->addFlash('success', sprintf(
+                <<<'FLASH'
+                Contact created successfully!
+                <br>
+                (reCAPTCHA threshold: %.2f, reCAPTCHA score: %.2f)
+                FLASH,
+                $recaptchaThreshold,
+                $recaptchaScore ?? 0.0
+            ));
 
             return $this->redirectToRoute('app_contact_index');
         }
